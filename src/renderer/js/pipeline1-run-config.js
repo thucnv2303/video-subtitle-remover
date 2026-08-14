92908ffbfabeb15364ce3736f817baf6222f345e
thucnv2303 <thuc.nv2303@gmail.com>
2026-08-14 21:33:31 +0700

    feat(P3): bootstrap subtitle resize and effects

--- src/renderer/js/pipeline1-run-config.js
+++ src/renderer/js/pipeline1-run-config.js
@@ -1,6 +1,7 @@
 import './pipeline1-run-ux.js';
 import './pipeline1-log-router.js';
 import './pipeline3/editor.js';
+import './pipeline3/subtitle-resize-effects.js';
 import { installPerJobSemanticRemixControls } from './pipeline1-semantic-remix-per-job.js';
 
 /**
